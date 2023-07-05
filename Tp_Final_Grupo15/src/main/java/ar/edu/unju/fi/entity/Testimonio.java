b23e5f554142d033a670778b31e5329759ec7b4f
Buscapina <Buscapina@DESKTOP-4GD11KH>
2023-07-04 22:03:50 -0300

    Creacion archivo testimonios.html

---
+++ Tp_Final_Grupo15/src/main/java/ar/edu/unju/fi/entity/Testimonio.java
@@ -0,0 +1,14 @@
+package ar.edu.unju.fi.entity;
+
+import java.time.LocalDate;
+
+public class Testimonio {
+
+	private Long id;
+	
+	private String usuario;
+	
+	private LocalDate fechaComentario;
+	
+	private String comentario;
+}
