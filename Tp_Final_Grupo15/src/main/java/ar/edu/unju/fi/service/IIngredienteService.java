0ceb283d2328ef11d02168992845b07aeca239fa
carlos ezequiel vilte <ecvilte@gmail.com>
2023-07-06 15:31:49 -0300

    creacion capa servicio de recetas e ingredientes

---
+++ Tp_Final_Grupo15/src/main/java/ar/edu/unju/fi/service/IIngredienteService.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import ar.edu.unju.fi.entity.Ingrediente;
+@Service
+public interface IIngredienteService {
+	public List<Ingrediente> getAllIngredientes();
+	public Ingrediente findIngredienteById(Long id);
+	public Ingrediente findIngrediente();
+}
---
+++ Tp_Final_Grupo15/src/main/java/ar/edu/unju/fi/service/imp/RecetaServiceImp.java
@@ -0,0 +1,51 @@
+package ar.edu.unju.fi.service.imp;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import ar.edu.unju.fi.entity.Receta;
+import ar.edu.unju.fi.repository.IRecetaRepository;
+import ar.edu.unju.fi.service.IRecetaService;
+
+@Service
+public class RecetaServiceImp implements IRecetaService{
+	
+	@Autowired
+	private IRecetaRepository recetaRepository;
+	
+	@Autowired
+	private Receta receta;
+	
+	@Override
+	public Receta getRecetaById(Long id) {
+		return recetaRepository.findById(id).get();
+	}
+
+	@Override
+	public Receta getReceta() {
+		return receta;
+	}
+
+	@Override
+	public void addReceta(Receta receta) {
+		recetaRepository.save(receta);
+	}
+
+	@Override
+	public void modifyReceta(Receta receta) {
+		recetaRepository.save(receta);
+	}
+
+	@Override
+	public void deleteReceta(Receta receta) {
+		recetaRepository.delete(receta);
+	}
+
+	@Override
+	public List<Receta> getList(String categoria) {
+		return recetaRepository.findByCategory(categoria);
+	}
+
+}
