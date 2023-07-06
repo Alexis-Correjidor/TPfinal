d27a4b8d2c42fb6f6fda7f065bb2b3e14f67f9b2
carlos ezequiel vilte <ecvilte@gmail.com>
2023-07-06 14:52:29 -0300

    creacion de repositorio para receta e ingredientes

---
+++ Tp_Final_Grupo15/src/main/java/ar/edu/unju/fi/repository/IIngredienteRepository.java
@@ -0,0 +1,9 @@
+package ar.edu.unju.fi.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import ar.edu.unju.fi.entity.Ingrediente;
+
+public interface IIngredienteRepository extends CrudRepository<Ingrediente, Long>{
+	
+}
