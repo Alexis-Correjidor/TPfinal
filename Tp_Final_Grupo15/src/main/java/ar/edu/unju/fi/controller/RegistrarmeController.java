c7dd2a6c5f82f9073c59e6e9aa159b8772664975
gonzanv <glskglskgk@gmail.com>
2023-07-09 01:10:51 -0300

    Se aplicaron los cambios para mostrar el codigo de user al crear usuario

--- Tp_Final_Grupo15/src/main/java/ar/edu/unju/fi/controller/RegistrarmeController.java
+++ Tp_Final_Grupo15/src/main/java/ar/edu/unju/fi/controller/RegistrarmeController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import ar.edu.unju.fi.entity.Usuario;
@@ -28,21 +29,29 @@ public class RegistrarmeController {
 	}
 	
 	@PostMapping("/guardar")
-	public ModelAndView saveNewProducto(
-			@Valid @ModelAttribute(value = "usuario") Usuario usuario,
-			BindingResult resultadoValidacion) {
+	public ModelAndView saveNewUsuario(
+	        @Valid @ModelAttribute(value = "usuario") Usuario usuario,
+	        BindingResult resultadoValidacion) {
 
-		ModelAndView modelAndView = new ModelAndView();
+	    ModelAndView modelAndView = new ModelAndView();
 
-		if (resultadoValidacion.hasErrors()) {
-			modelAndView.setViewName("registrarme");
-		} else {
-			usuarioService.guardar(usuario);
+	    if (resultadoValidacion.hasErrors()) {
+	        modelAndView.setViewName("registrarme");
+	    } else {
+	        usuarioService.guardar(usuario);
+	        modelAndView.setViewName("redirect:/registrarme/usuarioregistrado?id=" + usuario.getIdentificador());
+	    }
 
-			modelAndView.setViewName("redirect:/inicio");	
+	    return modelAndView;
+	}
+	
+	@GetMapping("/usuarioregistrado")
+	private ModelAndView getRegistradoPage(@RequestParam("id") long id) {
+	    Usuario usuario = usuarioService.getById(id);
 
-		}
+	    ModelAndView modelAndView = new ModelAndView("registrado");
+	    modelAndView.addObject("usuario", usuario);
 
-		return modelAndView;
+	    return modelAndView;
 	}
 }
